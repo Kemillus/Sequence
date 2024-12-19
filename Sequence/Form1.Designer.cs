bb6eba30581fbef24f46a6bbbd80c5b4fe8810c7
NNicke0de0n <vshnakenberg@mail.ru>
2024-12-19 22:51:44 +0500

    Initial commit

---
+++ Sequence/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Sequence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSequence = new System.Windows.Forms.Label();
+            this.listBoxItem = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxReceiptInndex = new System.Windows.Forms.TextBox();
+            this.buttonGetItem = new System.Windows.Forms.Button();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonInsertItem = new System.Windows.Forms.Button();
+            this.buttonGetSequence = new System.Windows.Forms.Button();
+            this.listBoxTransformation = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxInsertionIndex = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelStartIndex = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelSequence
+            // 
+            this.labelSequence.AutoSize = true;
+            this.labelSequence.Location = new System.Drawing.Point(22, 94);
+            this.labelSequence.Name = "labelSequence";
+            this.labelSequence.Size = new System.Drawing.Size(56, 13);
+            this.labelSequence.TabIndex = 0;
+            this.labelSequence.Text = "Sequence";
+            // 
+            // listBoxItem
+            // 
+            this.listBoxItem.FormattingEnabled = true;
+            this.listBoxItem.Location = new System.Drawing.Point(25, 110);
+            this.listBoxItem.Name = "listBoxItem";
+            this.listBoxItem.Size = new System.Drawing.Size(120, 186);
+            this.listBoxItem.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Receipt Index";
+            // 
+            // textBoxReceiptInndex
+            // 
+            this.textBoxReceiptInndex.Location = new System.Drawing.Point(287, 126);
+            this.textBoxReceiptInndex.Name = "textBoxReceiptInndex";
+            this.textBoxReceiptInndex.Size = new System.Drawing.Size(100, 20);
+            this.textBoxReceiptInndex.TabIndex = 3;
+            // 
+            // buttonGetItem
+            // 
+            this.buttonGetItem.Location = new System.Drawing.Point(287, 171);
+            this.buttonGetItem.Name = "buttonGetItem";
+            this.buttonGetItem.Size = new System.Drawing.Size(100, 23);
+            this.buttonGetItem.TabIndex = 4;
+            this.buttonGetItem.Text = "Get Item";
+            this.buttonGetItem.UseVisualStyleBackColor = true;
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(162, 126);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(100, 20);
+            this.textBoxValue.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Value";
+            // 
+            // buttonInsertItem
+            // 
+            this.buttonInsertItem.Location = new System.Drawing.Point(162, 210);
+            this.buttonInsertItem.Name = "buttonInsertItem";
+            this.buttonInsertItem.Size = new System.Drawing.Size(100, 23);
+            this.buttonInsertItem.TabIndex = 7;
+            this.buttonInsertItem.Text = "Insert Item";
+            this.buttonInsertItem.UseVisualStyleBackColor = true;
+            // 
+            // buttonGetSequence
+            // 
+            this.buttonGetSequence.Location = new System.Drawing.Point(546, 215);
+            this.buttonGetSequence.Name = "buttonGetSequence";
+            this.buttonGetSequence.Size = new System.Drawing.Size(120, 23);
+            this.buttonGetSequence.TabIndex = 8;
+            this.buttonGetSequence.Text = "Get Subsequence";
+            this.buttonGetSequence.UseVisualStyleBackColor = true;
+            // 
+            // listBoxTransformation
+            // 
+            this.listBoxTransformation.FormattingEnabled = true;
+            this.listBoxTransformation.Location = new System.Drawing.Point(404, 110);
+            this.listBoxTransformation.Name = "listBoxTransformation";
+            this.listBoxTransformation.Size = new System.Drawing.Size(120, 186);
+            this.listBoxTransformation.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(401, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Transformation";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(315, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "result";
+            // 
+            // textBoxInsertionIndex
+            // 
+            this.textBoxInsertionIndex.Location = new System.Drawing.Point(162, 174);
+            this.textBoxInsertionIndex.Name = "textBoxInsertionIndex";
+            this.textBoxInsertionIndex.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInsertionIndex.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(159, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Insertion Index";
+            // 
+            // labelStartIndex
+            // 
+            this.labelStartIndex.AutoSize = true;
+            this.labelStartIndex.Location = new System.Drawing.Point(543, 94);
+            this.labelStartIndex.Name = "labelStartIndex";
+            this.labelStartIndex.Size = new System.Drawing.Size(58, 13);
+            this.labelStartIndex.TabIndex = 15;
+            this.labelStartIndex.Text = "Start Index";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(546, 111);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(546, 172);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(546, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "End Index";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelStartIndex);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxInsertionIndex);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxTransformation);
+            this.Controls.Add(this.buttonGetSequence);
+            this.Controls.Add(this.buttonInsertItem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxValue);
+            this.Controls.Add(this.buttonGetItem);
+            this.Controls.Add(this.textBoxReceiptInndex);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxItem);
+            this.Controls.Add(this.labelSequence);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSequence;
+        private System.Windows.Forms.ListBox listBoxItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxReceiptInndex;
+        private System.Windows.Forms.Button buttonGetItem;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonInsertItem;
+        private System.Windows.Forms.Button buttonGetSequence;
+        private System.Windows.Forms.ListBox listBoxTransformation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxInsertionIndex;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelStartIndex;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
